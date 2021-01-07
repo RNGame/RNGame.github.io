f093f6f187daf88fa1b3c33a062d576b740ae773
MRudi95 <marvin.rudolph@htwg-konstanz.de>
2021-01-07 12:19:54 +0100

    trau mich

--- dist/build.js
+++ dist/build.js
@@ -46,6 +46,7 @@ let stars;
 let impacts;
 let sound_nom;
 let sound_oof;
+let eckangle;
 function preload() {
     earth.earthImage = loadImage("/res/earth.png");
     sound_nom = new p5.SoundFile("/res/nom.mp3");
@@ -63,6 +64,7 @@ function setup() {
     meteors = [];
     stars = [];
     impacts = [];
+    eckangle = atan2(height, width);
 }
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
@@ -76,7 +78,8 @@ function draw() {
     earth.draw();
     player.draw();
     stars.forEach(star => star.draw());
-    meteors.push(new Meteor(gamesize + gamesize / 2, random(PI * 2), width, height, earthSize, 50));
+    line(0, 0, width / 2, height / 2);
+    meteors.push(new Meteor(windowWidth + 400, random(PI * 2), width, height, earthSize, 50));
     meteors.forEach(meteor => {
         if (meteor.stateImpact) {
             removeMeteor(meteor);
@@ -97,14 +100,43 @@ function draw() {
         impact.draw();
     });
 }
+function line_intersection(p1, p2, p3, p4) {
+    let xdiff = [p1[0] - p2[0], p3[0] - p4[0]];
+    let ydiff = [p1[1] - p2[1], p3[1] - p4[1]];
+    function det(a, b) {
+        return a[0] * b[1] - a[1] * b[0];
+    }
+    let div = det(xdiff, ydiff);
+    let d = [det(p1, p2), det(p3, p4)];
+    let x = det(d, xdiff) / div;
+    let y = det(d, ydiff) / div;
+    return [x, y];
+}
+function berechnungstuff(angle, point) {
+    if (angle <= eckangle || angle >= 2 * PI - eckangle) {
+        return line_intersection([width / 2, height / 2], point, [width, 0], [width, height]);
+    }
+    else if (angle <= PI - eckangle) {
+        return line_intersection([width / 2, height / 2], point, [0, height], [width, height]);
+    }
+    else if (angle <= 1.5 * PI - (PI / 2 - eckangle)) {
+        return line_intersection([width / 2, height / 2], point, [0, 0], [0, height]);
+    }
+    else {
+        return line_intersection([width / 2, height / 2], point, [0, 0], [width, 0]);
+    }
+}
 class Meteor {
     constructor(radius, angle, width, height, earthsize, playersize) {
         this.earthX = width / 2;
         this.earthY = height / 2;
         this.startX = radius * cos(angle) + this.earthX;
         this.startY = radius * sin(angle) + this.earthY;
-        this.posX = this.startX;
-        this.posY = this.startY;
+        let schnittpunkt = berechnungstuff(angle, [this.startX, this.startY]);
+        this.posX = schnittpunkt[0];
+        this.posY = schnittpunkt[1];
+        this.startX = schnittpunkt[0];
+        this.startY = schnittpunkt[1];
         this.meteorSize = 25;
         this.earthsSize = earthsize / 2;
         this.playerSize = playersize / 2;
@@ -118,8 +150,10 @@ class Meteor {
             return;
         push();
         noStroke();
-        fill(255, 0, 0);
         ellipseMode(CENTER);
+        fill(255, 255, 0);
+        ellipse(this.startX, this.startY, 20);
+        fill(255, 0, 0);
         ellipse(this.posX, this.posY, this.meteorSize);
         const distX = this.startX - this.earthX;
         const distY = this.startY - this.earthY;
