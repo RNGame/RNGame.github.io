67a30368d69cdf4d077d43c7df5b9839b5299b12
MRudi95 <marvin.rudolph@htwg-konstanz.de>
2021-02-09 17:15:20 +0100

    better formatting!

--- src/z_main.ts
+++ src/z_main.ts
@@ -67,14 +67,7 @@ function draw() {
     frameCount % (framesPerSecond / meteorsPerSecond) === 0;
   if (shouldSpawnMeteor) {
     meteors.push(
-      new Meteor(
-        windowWidth + 400,
-        random(PI * 2),
-        width,
-        height,
-        earthSize,
-        50
-      )
+      new Meteor(windowWidth + 400, random(PI * 2), width, height, earthSize, 50)
     );
   }
 
@@ -105,12 +98,7 @@ function draw() {
 }
 
 //i will refactor this later
-function line_intersection(
-  p1: number[],
-  p2: number[],
-  p3: number[],
-  p4: number[]
-) {
+function line_intersection(p1: number[], p2: number[], p3: number[], p4: number[]) {
   let xdiff = [p1[0] - p2[0], p3[0] - p4[0]];
   let ydiff = [p1[1] - p2[1], p3[1] - p4[1]];
 
@@ -132,35 +120,15 @@ function line_intersection(
 function berechnungstuff(angle: number, point: number[]) {
   if (angle <= eckangle || angle >= 2 * PI - eckangle) {
     //rechte wand
-    return line_intersection(
-      [width / 2, height / 2],
-      point,
-      [width, 0],
-      [width, height]
-    ); // earth, meteorstart, wand_p1, wand_p2
+    return line_intersection([width / 2, height / 2], point, [width, 0], [width, height]); // earth, meteorstart, wand_p1, wand_p2
   } else if (angle <= PI - eckangle) {
     //untere wand
-    return line_intersection(
-      [width / 2, height / 2],
-      point,
-      [0, height],
-      [width, height]
-    );
+    return line_intersection([width / 2, height / 2], point, [0, height], [width, height]);
   } else if (angle <= 1.5 * PI - (PI / 2 - eckangle)) {
     //linke wand
-    return line_intersection(
-      [width / 2, height / 2],
-      point,
-      [0, 0],
-      [0, height]
-    );
+    return line_intersection([width / 2, height / 2], point, [0, 0], [0, height]);
   } else {
     //obere wand
-    return line_intersection(
-      [width / 2, height / 2],
-      point,
-      [0, 0],
-      [width, 0]
-    );
+    return line_intersection([width / 2, height / 2], point, [0, 0], [width, 0]);
   }
 }
