c1de0937d831802fec3c699ab0cf0f573c31704b
MRudi95 <48942024+MRudi95@users.noreply.github.com>
2021-02-11 16:06:35 +0000

    Deploying to gh-pages from @ RNGame/RNGame.github.io@fe379d9140b9645883ad79b819ca38bafa96792d 🚀

--- ts/models/meteor.d.ts
+++ ts/models/meteor.d.ts
@@ -1,18 +1,24 @@
 import p5 from "p5";
 export declare class Meteor {
-    constructor(radius: number, angle: number, width: number, height: number, earthsize: number, playersize: number, image: p5.Image);
-    startX: number;
-    startY: number;
+    constructor(rng_values: {
+        angle?: number;
+        speed?: number;
+        size?: number;
+    }, width: number, //width of the screen
+    height: number, //height of the screen
+    earthsize: number, playersize: number, image: p5.Image);
     posX: number;
     posY: number;
     earthX: number;
     earthY: number;
+    distX: number;
+    distY: number;
     meteorSize: number;
     earthsSize: number;
     playerSize: number;
     stateImpact: boolean;
     stateEaten: boolean;
-    factor: number;
+    speedFactor: number;
     image: p5.Image;
     private eckangle;
     draw(p: p5): void;
