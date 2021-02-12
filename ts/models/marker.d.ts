9cb5710c3880fb5e11c66862fca5efec20f242da
MRudi95 <48942024+MRudi95@users.noreply.github.com>
2021-02-12 14:37:45 +0000

    Deploying to gh-pages from @ RNGame/RNGame.github.io@2562f6f07ef114d98994199cb632450866f502bc 🚀

--- ts/models/marker.d.ts
+++ ts/models/marker.d.ts
@@ -28,10 +28,29 @@ export declare class Markerlist {
     south: Marker[];
     east: Marker[];
     west: Marker[];
+    secondarymarkers: SecondaryMarkerlist;
     color: p5.Color;
     draw(p: p5): void;
     push(marker: Marker): void;
     private pushtolist;
     private mergemarker;
+}
+declare class SecondaryMarker {
+    constructor(x: number, y: number, size: number);
+    posX: number;
+    posY: number;
+    size: number;
+    counter: number;
+    opacity: number;
+    animationlength: number;
+    stateFinished: boolean;
+    draw(p: p5, color: p5.Color): void;
+}
+declare class SecondaryMarkerlist {
+    constructor();
+    markers: SecondaryMarker[];
+    draw(p: p5, color: p5.Color): void;
+    push(marker: SecondaryMarker): void;
+    private removemarker;
 }
 export {};
